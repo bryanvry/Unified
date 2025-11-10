3ec2983e6822b859a21b2dce58b27dfadbccbfd2
bryanvry <bryanvrya@gmail.com>
2025-11-10 02:08:17 -0800

    Update __init__.py

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,6 +1,10 @@
-
-from .unified_parser import UnifiedParser
+# parsers/__init__.py
 from .southern_glazers import SouthernGlazersParser
 from .nevada_beverage import NevadaBeverageParser
 
-ALL_PARSERS = [UnifiedParser(), SouthernGlazersParser(), NevadaBeverageParser()]
+ALL_PARSERS = {
+    "southern_glazers": SouthernGlazersParser,
+    "nevada_beverage": NevadaBeverageParser,
+}
+
+__all__ = ["ALL_PARSERS", "SouthernGlazersParser", "NevadaBeverageParser"]
