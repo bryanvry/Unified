1fc476be3fc0754962cbc4421fd3c8364d6eda63
bryanvry <bryanvrya@gmail.com>
2025-11-10 00:09:32 -0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-
-from .unified_parser import UnifiedParser
-from .southern_glazers import SouthernGlazersParser
-from .nevada_beverage import NevadaBeverageParser
-
-ALL_PARSERS = [UnifiedParser(), SouthernGlazersParser(), NevadaBeverageParser()]
