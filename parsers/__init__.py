2e0e2c545ca4eb60c2199c0f47c00615a8537d7f
bryanvry <bryanvrya@gmail.com>
2025-11-12 04:25:37 -0800

    Update __init__.py

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,12 +1,6 @@
-# parsers/__init__.py
-# Pre-Breakthru registry (Unified, SG, Nevada only)
 
 from .unified_parser import UnifiedParser
 from .southern_glazers import SouthernGlazersParser
 from .nevada_beverage import NevadaBeverageParser
 
-ALL_PARSERS = {
-    "Unified (SVMERCH)": ("unified", UnifiedParser),
-    "Southern Glazer's": ("southern_glazers", SouthernGlazersParser),
-    "Nevada Beverage": ("nevada_beverage", NevadaBeverageParser),
-}
+ALL_PARSERS = [UnifiedParser(), SouthernGlazersParser(), NevadaBeverageParser()]
