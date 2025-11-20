1326bbe6cc9772cecedc6aa51a8042fa43d2dfb8
bryanvry <bryanvrya@gmail.com>
2025-11-20 03:59:32 -0800

    Update __init__.py

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,6 +1,11 @@
-# parsers/__init__.py
 from .southern_glazers import SouthernGlazersParser
 from .nevada_beverage import NevadaBeverageParser
-from .breakthru import BreakthruParser  # NEW
+from .breakthru import BreakthruParser
+from .jcsales import JCSalesParser  # ← add this
 
-__all__ = ["SouthernGlazersParser", "NevadaBeverageParser", "BreakthruParser"]
+__all__ = [
+    "SouthernGlazersParser",
+    "NevadaBeverageParser",
+    "BreakthruParser",
+    "JCSalesParser",  # ← and this
+]
