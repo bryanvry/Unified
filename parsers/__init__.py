ecd9160b47b85010b681eea8228360b7d91a366c
bryanvry <bryanvrya@gmail.com>
2026-02-18 00:32:01 -0800

    Update __init__.py

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -2,12 +2,14 @@ from .southern_glazers import SouthernGlazersParser
 from .nevada_beverage import NevadaBeverageParser
 from .breakthru import BreakthruParser
 from .jcsales import JCSalesParser
-from .costco import CostcoParser  # <--- Added
+from .costco import CostcoParser
+from .unified_parser import UnifiedParser 
 
 __all__ = [
     "SouthernGlazersParser",
     "NevadaBeverageParser",
     "BreakthruParser",
     "JCSalesParser",
-    "CostcoParser",  # <--- Added
+    "CostcoParser",
+    "UnifiedParser", 
 ]
